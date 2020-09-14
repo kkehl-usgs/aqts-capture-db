9ac2ac50305b1be67fa6a48006f3ede20ce06e43
kkehl-usgs <kkehl@contractor.usgs.gov>
2020-09-14 15:48:19 -0700

    try again

--- liquibase/scripts/dbInit/z2_rc_liquibase.sh
+++ liquibase/scripts/dbInit/z2_rc_liquibase.sh
@@ -5,10 +5,10 @@ set -o pipefail
 
 if [[ ${AQTS_DATABASE_ADDRESS} == *"amazon"* ]]; then
   psql -h ${AQTS_DATABASE_ADDRESS} -U ${AQTS_SCHEMA_OWNER_USERNAME} -d ${AQTS_DATABASE_NAME} -c "create extension if not exists aws_s3 cascade;"
-  echo "aws_s3 extension created!"
+  printf 'aws_s3_extension created!'
   exit 64
 else
-  echo "did not create aws_s3 extension because we are not using RDS"
+  printf 'did not create aws_s3 extension because we are not using RDS'
   exit 125
 fi
 
